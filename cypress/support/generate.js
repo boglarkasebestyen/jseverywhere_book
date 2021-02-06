b7a2519ad4aa852ff09d53b52c36b2951fd0c9d6
Boglárka Sebestyén <22778918+boglarkasebestyen@users.noreply.github.com>
2021-02-05 17:41:08 -0800

    Delete cypress

--- cypress/support/generate.js
+++
@@ -1,9 +0,0 @@
-import { build, fake } from 'test-data-bot';
-
-const userBuilder = build('User').fields({
-  username: fake(f => f.internet.userName()),
-  email: fake(f => f.internet.email()),
-  password: fake(f => f.internet.password())
-});
-
-export { userBuilder };
